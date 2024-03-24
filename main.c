ab1979b99e4490c6fcb9f4b4e027e1b95fc4e476
Eldalt <juaan.mantecon@hotmail.com>
2024-03-24 00:59:16 -0300

    SWAP COMMIT 2

--- main.c
+++ main.c
@@ -48,12 +48,5 @@ int main(int argc, char *argv[])
 	return 0;
 }
 
-void intercambio(struct v *a) {
-	int i, aux;
-	for (i = 0; i < 5; i++) {
-		aux = a->z1[i];
-		a->z1[i] = a->z2[i];
-		a->z2[i] = aux;
-	}
-}
+
 
